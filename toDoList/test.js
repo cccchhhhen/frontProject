33fbb13b2c60df6e774c967392f382039a01fb51
CHENNANY <1366190531@qq.com>
2025-02-18 17:54:56 +0800

    checkbox status

---
+++ toDoList/test.js
@@ -0,0 +1,4 @@
+let a = {'a':1};
+console.log(a);
+Object.assign(a,{'b':2});
+console.log(a);
--- toDoList/todo.js
+++ toDoList/todo.js
@@ -4,6 +4,7 @@ if(!window.localStorage) {
     // return false;
 }
 else{
+    localStorage.clear();
     console.log('支持本地存储');
 }
 var enter = document.querySelector('#inputBox');
@@ -27,9 +28,9 @@ function addItems(){
         return;
     }
     let key = pre + new Date().valueOf();
-    let value = enter.value;
-
-    localStorage.setItem(key,value);
+    let value = {'value':enter.value};
+    
+    localStorage.setItem(key,JSON.stringify(value));
     createItem(key);
 }
 function createItem(key){
@@ -43,8 +44,7 @@ function createItem(key){
 
     // 创建关联标签
     var label = document.createElement('label');
-    // label.textContent = enter.value;
-    label.textContent = localStorage.getItem(key);
+    label.textContent = JSON.parse(localStorage.getItem(key)).value;
 
     // 创建删除按钮
     var del = document.createElement('span');
@@ -55,6 +55,11 @@ function createItem(key){
         perListBox.remove();
     };
 
+    let newValue = JSON.parse(localStorage.getItem(key));
+    // console.log('newKey1',newKey);
+    Object.assign(newValue,{'checked':false});
+    // console.log('newKey',newKey);
+    localStorage.setItem(key,JSON.stringify(newValue))
     // 复选框事件绑定
     input.onchange = function() {
         toggleList(key,this);
@@ -71,6 +76,12 @@ function createItem(key){
 
 // 切换复选框状态(勾选、不勾选)
 function toggleList(key,checkbox){
+    let value = JSON.parse(localStorage.getItem(key));
+    // console.log('checked',checked);
+    value.checked = !value.checked;
+    
+    // let status = !localStorage.key.checked;
+    localStorage.setItem(key,JSON.stringify(value));
     var label = checkbox.parentElement.querySelector('label');
     label.classList.toggle('strike-through',checkbox.checked);
 }
