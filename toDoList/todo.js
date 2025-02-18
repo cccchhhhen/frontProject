43ab40a6b30cbc400e7bb5ec959636f3c50db543
CHENNANY <1366190531@qq.com>
2025-02-18 17:11:23 +0800

    finish localStorage

--- toDoList/todo.js
+++ toDoList/todo.js
@@ -1,11 +1,38 @@
+// 判断浏览器是否支持本地存储
+if(!window.localStorage) {
+    alert('不支持本地存储!');
+    // return false;
+}
+else{
+    console.log('支持本地存储');
+}
+var enter = document.querySelector('#inputBox');
+var list_container = document.querySelector('#list-container');
+const pre = 'toDoList ';
+
+toDoListInit();
+function toDoListInit(){
+    // var flag = true;
+    if(localStorage.length>0){
+        for(let i=0;i<localStorage.length;i++){
+            if(localStorage.key(i).slice(0,pre.length) === pre){
+                createItem(localStorage.key(i));
+            }
+        }
+    }
+}
 function addItems(){
-    var enter = document.querySelector('#inputContent');
-    var list_container = document.querySelector('#list-container');
     if(enter.value.trim() === ''){ // 如果没有输入待办事项
         alert('Please enter the to-do items.');
         return;
     }
-    
+    let key = pre + new Date().valueOf();
+    let value = enter.value;
+
+    localStorage.setItem(key,value);
+    createItem(key);
+}
+function createItem(key){
     // 创建容器
     var perListBox = document.createElement('div');
     perListBox.className = 'list-item';
@@ -16,21 +43,22 @@ function addItems(){
 
     // 创建关联标签
     var label = document.createElement('label');
-    label.textContent = enter.value;
+    // label.textContent = enter.value;
+    label.textContent = localStorage.getItem(key);
 
     // 创建删除按钮
     var del = document.createElement('span');
     del.className = 'del-btn';
     del.textContent = '×';
     del.onclick = function() {
+        localStorage.removeItem(key);
         perListBox.remove();
     };
 
-    // 事件绑定
+    // 复选框事件绑定
     input.onchange = function() {
-        finishList(this);
+        toggleList(key,this);
     };
-
 
     // 组装元素
     perListBox.appendChild(input);
@@ -39,9 +67,11 @@ function addItems(){
     list_container.appendChild(perListBox);
     // 清除输入框
     enter.value = '';
-    
 }
-function finishList(checkbox){
+
+// 切换复选框状态(勾选、不勾选)
+function toggleList(key,checkbox){
     var label = checkbox.parentElement.querySelector('label');
     label.classList.toggle('strike-through',checkbox.checked);
 }
+
